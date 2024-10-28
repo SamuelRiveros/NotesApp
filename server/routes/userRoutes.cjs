bb71f3f79061c8c64ba958d191eed2a68455c95b
SamuelRiveros <156517817+SamuelRiveros@users.noreply.github.com>
2024-10-27 20:07:14 -0500

    feat: :sparkles: Login Funcional, maneja el jwt

--- server/routes/userRoutes.cjs
+++ server/routes/userRoutes.cjs
@@ -4,13 +4,8 @@ const userController = require('../controllers/userController.cjs');
 const auth = require('../middlewares/auth.cjs'); // Middleware de autenticación JWT
 const rateLimiter = require('../middlewares/rateLimiter.cjs'); // Middleware de límite de solicitudes
 
-// Rutas públicas
-router.post('/',rateLimiter.postLimiter, userController.crear);
-router.post('/iniciarSesion',rateLimiter.loginLimiter, userController.iniciarSesion);
-
-// Rutas públicas
-router.post('/', auth, rateLimiter.postLimiter, userController.crear);
-router.post('/login', auth, rateLimiter.loginLimiter, userController.iniciarSesion);
+router.post('/', rateLimiter.postLimiter, userController.crear);
+router.post('/login', rateLimiter.loginLimiter, userController.iniciarSesion);
 router.post('/logout', auth, userController.cerrarSesion);
 
 module.exports = router
